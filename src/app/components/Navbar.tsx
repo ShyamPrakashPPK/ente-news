1758d0a932334cc19802e237ece182961e8d80d0
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2024-01-15 11:19:17 +0530

    navbar link to hero page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import { useState } from 'react'
 
 export default function Navbar() {
@@ -6,7 +7,9 @@ export default function Navbar() {
     return (
         <header className="bg-[#151415] border-b-[1px] border-gray-500">
             <nav className="    p-6 lg:px-8" aria-label="Global">
-                <h1 className='text-center text-3xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-br  from-green-500 to-lime-500 hover:text-white transition-colors duration-300 ease-in-out font-extrabold'>ente news</h1>
+                <Link href="/">
+                    <h1 className='text-center text-3xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-br  from-green-500 to-lime-500 hover:text-white transition-colors duration-300 ease-in-out font-extrabold'>ente news</h1>
+                </Link>
             </nav>
         </header>
     )
